c275c6e6bd0ca0e1f9a00a66d275ae477a15f204
Qurban Yusufov <qurbanyusifov89@gmail.com>
2024-08-07 12:05:46 +0400

    main text changed

--- app/src/main/java/com/example/github/MainActivity.kt
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -7,7 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         println("Master")
+
+        println("Deneme123")
+
+        for(i in 1..10){
+            println(i)
+        }
     }
 }
